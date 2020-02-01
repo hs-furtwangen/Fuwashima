0434772ad8144dcd75af540299905adfd0910f29
Adrian Kubicek <adrian.kubicek@web.de>
2020-02-01 22:25:54 +0100

    UI with Start Scene, Audio Scripts and Siren Prefab
    - Added Starting Scene with UI. Needs content, but I will do that tomorrow
    - Audio Scripts for Components and non directional Sound Effects, like background music
    - I added a Siren Script and it is able to rotate. The Materials need work however

---
+++ Unity/Assets/Materials&Scripts_Adrian/KomponentSoundeffects.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KomponentSoundeffects : MonoBehaviour
+{
+    public enum ComponentSoundeffects { SirensAlarm, buttonPressed, crankPulled, nrOfElements}
+
+    public AudioSource myAudioSource = null;
+    public List<AudioClip> componentSoundEffectList;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myAudioSource = GetComponent<AudioSource>();
+    }
+
+    public void preLoadAudioSources()
+    {
+        for (int i = 0; i < (int)ComponentSoundeffects.nrOfElements; i++)
+        {
+            componentSoundEffectList.Add((AudioClip)Resources.Load(switchComponentSoundEffects((ComponentSoundeffects)i)));
+        }
+    }
+
+    public void setComponentSoundEffects(ComponentSoundeffects incomingEnum)
+    {
+        myAudioSource.clip = componentSoundEffectList[(int)incomingEnum];
+        myAudioSource.Play();
+    }
+
+    public string switchComponentSoundEffects(ComponentSoundeffects incomingEnum)
+    {
+        string convertedString = "";
+        switch (incomingEnum)
+        {
+            case ComponentSoundeffects.SirensAlarm:
+                convertedString = "Audio/Components/" + "SirensAlarm";
+                return convertedString;
+            case ComponentSoundeffects.buttonPressed:
+                convertedString = "Audio/Components/" + "buttonPressed";
+                return convertedString;
+            case ComponentSoundeffects.crankPulled:
+                convertedString = "Audio/Components/" + "crankPulled";
+                return convertedString;
+        }
+        return convertedString;
+    }
+}
---
+++ Unity/Assets/Materials&Scripts_Adrian/UISplashScreenBehaviour.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UISplashScreenBehaviour : MonoBehaviour
+{
+    public enum ScreenTitles { Title, Instruction1, Instruction2, Instruction3}
+
+    private Button instructionStartButton = null;
+    public Button[] allOtherButtons;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        allOtherButtons = GetComponentsInChildren<Button>();
+        instructionStartButton = this.transform.GetChild(0).GetChild(2).GetComponent<Button>();
+        instructionStartButton.onClick.AddListener(openFirstInstructionPanel);
+
+        for (int i = 0; i < this.transform.childCount; i++)
+        {
+            if (i == 0)
+            {
+                this.transform.GetChild(i).gameObject.SetActive(true);
+            }
+            else
+            {
+                this.transform.GetChild(i).gameObject.SetActive(false);
+            }
+            
+        }
+    }
+
+    public void screenSwitcher(ScreenTitles incomingScreenTitle)
+    {
+        for (int i = 0; i < this.transform.childCount; i++)
+        {
+            this.transform.GetChild(i).gameObject.SetActive(false);
+        }
+
+        this.transform.GetChild((int)incomingScreenTitle).gameObject.SetActive(true);
+    }
+
+    public void openFirstInstructionPanel()
+    {
+        screenSwitcher(ScreenTitles.Instruction1);
+    }
+
+    public void openSecondInstructionPanel()
+    {
+        screenSwitcher(ScreenTitles.Instruction2);
+    }
+
+    public void openThirdInstructionPanel()
+    {
+        screenSwitcher(ScreenTitles.Instruction3);
+    }
+
+
+}
