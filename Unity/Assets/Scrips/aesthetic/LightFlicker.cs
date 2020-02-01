65ede167e5de8078cf574950c609c06ae78d6344
SimonStorlSchulke <simonstoschu@googlemail.com>
2020-02-01 20:45:45 +0100

    scene update, light flickering

---
+++ Unity/Assets/Scrips/aesthetic/LightFlicker.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+[RequireComponent (typeof(Light))]
+public class LightFlicker : MonoBehaviour
+{
+    Light lt;
+
+    [SerializeField]
+    public float intensityMultiplyer = 5, timeScale = 5, flickerFactor = 0.5f;
+
+    void Start()
+    {
+        lt = GetComponent<Light>();
+    }
+
+    void Update()
+    {
+        float noise = Mathf.PerlinNoise(Time.time * timeScale, 1);
+        lt.intensity = Mathf.Lerp(1, noise, flickerFactor) * intensityMultiplyer;
+    }
+}
